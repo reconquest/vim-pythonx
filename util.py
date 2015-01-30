f6384ca236221af6699ad279bb92d3faa37615e3
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-01-30 12:54:14 +0600

    port for php

--- util.py
+++ util.py
@@ -53,3 +53,12 @@ def convert_camelcase_to_snakecase(name):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
 
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+def get_cword():
+    return vim.eval('expand("<cword>")')
+
+def paste(content):
+    vim.command('set paste')
+    vim.command('normal i' + content)
+    vim.command('set nopaste')
