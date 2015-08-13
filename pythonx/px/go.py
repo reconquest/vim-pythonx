2ba879a3b54997904206b5882e882f6922fb56b3
Stanislav Seletskiy <s.seletskiy@gmail.com>
2015-08-13 12:50:56 +0600

    go: do not autoimport in strings

--- pythonx/px/go.py
+++ pythonx/px/go.py
@@ -167,6 +167,9 @@ def is_struct_bracket(buffer, line, column):
 
 
 def autoimport():
+    if all.is_syntax_string(vim.current.window.cursor):
+        return
+
     info = vim.eval('go#complete#GetInfo()')
     if info != "" and re.match("^var \w+", info):
         return
