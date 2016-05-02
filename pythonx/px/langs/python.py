7611ede5f108a238de43ed44bea886d6425d4e5e
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-05-03 03:08:13 +0600

    python: fix basic code

--- pythonx/px/langs/python.py
+++ pythonx/px/langs/python.py
@@ -1,38 +1,40 @@
 # coding=utf8
 
-# import vim
-# import re
-# import px.all
-# import px.util
+import vim
+import re
+import px.whitespaces
+import px.buffer
 
-# function_re = re.compile('^def ')
-# method_re = re.compile('^\s+def ')
-# class_re = re.compile('^class ')
-
-# def ensure_newlines(buffer, cursor):
-    # line_number, _ = cursor
-    # line = buffer[line_number]
-    # if function_re.match(line):
-        # px.all.ensure_newlines(buffer, (line_number, 0), 2)
-    # if method_re.match(line):
-        # px.all.ensure_newlines(buffer, (line_number, 0), 1)
-    # if class_re.match(line):
-        # px.all.ensure_newlines(buffer, (line_number, 0), 2)
+function_re = re.compile('^def ')
+method_re = re.compile('^\s+def ')
+class_re = re.compile('^class ')
 
 
-# def ensure_newlines_after(buffer, cursor):
-    # x, line_number = px.util.get_next_nonempty_line(buffer, cursor[0])
-    # if line_number <= 0:
-        # return
-    # else:
-        # ensure_newlines(buffer, (line_number, 0))
+def ensure_newlines(buffer, cursor):
+    line_number, _ = cursor
+    line = buffer[line_number]
+    if function_re.match(line):
+        px.whitespaces.ensure_newlines(buffer, (line_number, 0), 2)
+    if method_re.match(line):
+        px.whitespaces.ensure_newlines(buffer, (line_number, 0), 1)
+    if class_re.match(line):
+        px.whitespaces.ensure_newlines(buffer, (line_number, 0), 2)
 
-# def ensure_indent(buffer, cursor, indent):
-    # if vim.eval('&et') == "1":
-        # indent_symbol = ' ' * int(vim.eval('&sw'))
-    # else:
-        # indent_symbol = '\t'
 
-    # buffer[cursor[0]] = indent_symbol * indent
+def ensure_newlines_after(buffer, cursor):
+    x, line_number = px.buffer.get_next_nonempty_line(buffer, cursor[0])
+    if line_number <= 0:
+        return
+    else:
+        ensure_newlines(buffer, (line_number, 0))
 
-    # return (cursor[0], len(buffer[cursor[0]]))
+
+def ensure_indent(buffer, cursor, indent):
+    if vim.eval('&et') == "1":
+        indent_symbol = ' ' * int(vim.eval('&sw'))
+    else:
+        indent_symbol = '\t'
+
+    buffer[cursor[0]] = indent_symbol * indent
+
+    return (cursor[0], len(buffer[cursor[0]]))
