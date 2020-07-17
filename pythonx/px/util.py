3bb8d014a2446f6b4b959c3787773bbceffe858b
Stanislav Seletskiy <s.seletskiy@gmail.com>
2020-07-17 12:22:26 +0300

    px.util: add convert_camelcase_to_kebabcase

--- pythonx/px/util.py
+++ pythonx/px/util.py
@@ -9,6 +9,12 @@ def convert_camelcase_to_snakecase(name):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 
+def convert_camelcase_to_kebabcase(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', name)
+
+    return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s1).lower()
+
+
 def remove_prefix(text, prefix):
     if text.startswith(prefix):
         return text[len(prefix):]
